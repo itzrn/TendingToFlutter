2ab672dd6231f356220e9dcfb4512e0c10c971f6
Aryan Prajapati <aryangithub2017@gmail.com>
2023-05-07 21:55:28 +0530

    update
    commit

--- serverconnection/lib/constant.dart
+++ serverconnection/lib/constant.dart
@@ -1,4 +1,4 @@
 // ignore_for_file: constant_identifier_names
 
-const MONDODB_URL = "mongodb+srv://<username>:<password>@cluster01.0kfe8io.mongodb.net/<Database Name>?retryWrites=true&w=majority";
+const MONDODB_URL = "mongodb+srv://<username>:<password>@<cluster name>.0kfe8io.mongodb.net/<Database Name>?retryWrites=true&w=majority";
 const COLLECTION_NAME = "<Collection Name>";
